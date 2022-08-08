cd53a2ab677deb72e8cab9d1f7233b77d49801fd
rainerdpaul <99883336+rainerdpaul@users.noreply.github.com>
2022-08-08 17:50:49 -0400

    Update KarpStrong.py

--- V1/KarpStrong.py
+++ V1/KarpStrong.py
@@ -13,8 +13,44 @@ from Effects import *
 setup = open("Setup.txt", "r")
 Fs = int(setup.readlines()[1])
 
-WAVEFORM = None
-OUTSTER = None
+# WAVEFORM = None
+# OUTMONO = None
+# OUTSTER = None
+# t = np.linspace(0., 1., Fs)
+
+
+# Frequency in hz, length in seconds, model selection between "Noise" or "Ints"
+# timbre only used in "noise" model
+# fund_mult takes [0,1] accentuates the fundamental frequency (hz)
+def ks_waveform(hz, model=None, timbre=1, fund_mult=0):
+    excitation_len = int(np.ceil(Fs / hz))
+
+    if model == "noise":
+        wave = np.random.normal(0, 1, excitation_len) ** (timbre // 2 * 2 + 1)
+        # normalize to float32 bounds
+        wave -= (max(wave) + min(wave)) / 2
+        wave *= 1 / (max(wave))
+        out = wave.astype(np.float32)
+    else:
+        wave = np.random.randint(0, 2, excitation_len) * 2 - 1
+        out = wave.astype(np.float32)
+
+    for i in range(len(out)):
+        out[i] = (out[i] * (1 - (i / len(out)))) + (out[i] * ((1 - fund_mult) * (i / len(out))))
+
+    return out
+
+
+# take waveform and repeat over time in seconds
+def ks_length(waveform, seconds):
+    out = waveform
+    num_samples = int(np.ceil(Fs * seconds))
+    while len(out) < num_samples:
+        out = np.append(out, waveform)
+    # 2-Channel output
+    out = np.insert(np.expand_dims(out[0:num_samples], axis=0), 1, out[0:num_samples], axis=0)
+    return  out
+
 
 # Create or access existing save.dat file
 # Used for updating file number for Sound#.wav
@@ -27,39 +63,16 @@ def filenum(filename="saveddata.dat"):
         f.write(str(val + 1))
         return val
 
-# Frequency in hz, length in seconds, model selection between "noise" or "ints"
-# timbre only used in "noise" model
-def karpstrong(hz, seconds, model=None, timbre=1):
-    excitation_len = int(np.ceil(Fs / hz))
-    num_samples = int(np.ceil(Fs * seconds))
 
-    if model == "noise":
-        wave = np.random.normal(0, 1, excitation_len) ** (timbre // 2 * 2 + 1)
-        # normalize to float32 bounds
-        wave -= (max(wave) + min(wave)) / 2
-        wave *= 1 / (max(wave))
-        out = wave.astype(np.float32)
-    else:
-        wave = np.random.randint(0, 2, excitation_len) * 2 - 1
-        out = wave.astype(np.float32)
-
-    waveform = out
-
-    while len(out) < num_samples:
-        out = np.append(out, wave)
-
-    # 2-Channel output
-    out = np.insert(np.expand_dims(out[0:num_samples], axis=0), 1, out[0:num_samples], axis=0)
-    return waveform, out
-
-
+# export as float32 wav file
 def to_wav(arr):
     arr = np.asarray(np.rot90(np.fliplr(arr)))
     write("Sound" + str(filenum()) + ".wav", Fs, arr)
 
 
-# Example
-WAVEFORM, OUTSTER = karpstrong(60, 1, "noise", 1)
-OUTSTER = haas_effect(OUTSTER, 160, "Left")
-OUTSTER = gain(OUTSTER, 0.3)
+# example
+OUTSTER = ks_length(ks_waveform(60, "Noise", 1, 0.5), 1)
+OUTSTER = gain(OUTSTER, 0.7)
+OUTSTER = adsr(OUTSTER, 80, 400, 150, 0.4, 200, 2, 1.3, 4)
+OUTSTER = haas_effect(OUTSTER, 32.4, "Left")
 to_wav(OUTSTER)
